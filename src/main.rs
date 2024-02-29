dac6bc614fac02df69f04fa56781b4048b85ae3c
Kilerd Chan <blove694@gmail.com>
2024-02-29 14:30:07 +0800

    fix: remove pilot and use teloxide

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@
 extern crate log;
 
 use std::borrow::Cow;
-
-use crate::github::upload_to_github;
-use once_cell::sync::{Lazy, OnceCell};
-use pilot::method::SendMediaGroup;
-use pilot::{bot::Bot, method::send::SendMessage, method::DeleteMessage, typing::UpdateMessage};
 use std::sync::Arc;
+
+use once_cell::sync::{Lazy, OnceCell};
+use teloxide::prelude::*;
+use teloxide::RequestError;
 use tokio::sync::mpsc;
 use tokio::sync::mpsc::{Receiver, Sender};
 use tokio::sync::mpsc::error::SendError;
 use tokio::time::Duration;
+
+use crate::github::upload_to_github;
 
 pub mod github;
 
@@ -23,7 +24,6 @@ pub struct Opts {
     repo: String,
 }
 
-static CHANNEL: OnceCell<Sender<(Arc<Bot>, Arc<UpdateMessage>)>> = OnceCell::new();
 
 #[tokio::main]
 async fn main() {
@@ -41,69 +41,51 @@ async fn main() {
     debug!("github repo is {}", opt.repo);
     std::env::set_var("TELEGRAM_BOT_SECRET_KEY", &opt.telegram_token);
 
-    let (sender, mut receiver) = tokio::sync::mpsc::channel(100);
+    let bot = Bot::new(&opt.telegram_token);
 
-    CHANNEL.set(sender).expect("cannot set global channel");
-    let mut bot = Bot::new();
+    info!("bot is listening");
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+       let opt = Arc::new(Opts {
+            chat: std::env::var("CHAT").expect("CHAT must be set"),
+            telegram_token: std::env::var("TELEGRAM_TOKEN").expect("TELEGRAM_TOKEN must be set"),
+            github_token: std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN must be set"),
+            repo: std::env::var("REPO").expect("REPO must be set"),
+        });
+        let chat_id = msg.chat.id;
+        let chat_whitelist = std::env::var("CHAT").unwrap();
 
-    bot.other(|bot, msg| async move {
-        debug!("bot get msg");
-        match CHANNEL.get().unwrap().send((bot, msg)).await {
-            Ok(_) => {}
-            Err(e) => {
-                error!("cannot send msg to channel: {}", e);
-            }
-        };
-    });
+        if !chat_id.to_string().eq(&chat_whitelist) {
+            warn!("sender[{}] does not equal to whitelist", &chat_id);
+            bot.send_message(chat_id, "your are not in whitelist").await?;
+            return Ok(());
+        }
 
-    let consumer = tokio::spawn(async move {
-        while let Some((bot, msg)) = receiver.recv().await {
-            match msg.as_ref() {
-                UpdateMessage::Message(new_msg) => {
-                    let chat_id = new_msg.chat.as_ref().id.to_string();
-
-                    let chat_whitelist = std::env::var("CHAT").unwrap();
-                    if !chat_id.eq(&chat_whitelist) {
-                        warn!("sender[{}] does not equal to whitelist", &chat_id);
-                        let reply = SendMessage::new(&chat_id, "your are not in whitelist");
-                        bot.request(reply).await;
-                        return;
-                    }
-
-                    if let Some(text) = &new_msg.text {
-                        let msg_id = new_msg.message_id;
-                        let msg_date = new_msg.date;
+        if let Some(text) = msg.text() {
+            let msg_id = msg.id;
+            let msg_date = msg.date.timestamp() as i32;
+            loop {
+                let result = upload_to_github(opt.clone(), text.to_string(), msg_date).await;
+                match result {
+                    Ok(_) => {
                         loop {
-                            let result = upload_to_github(opt.clone(), text.clone(), msg_date).await;
-                            match result {
-                                Ok(_) => {
-                                    loop {
-                                        info!("deleting telegram msg id={}", &msg_id);
-                                        let delete_message = DeleteMessage {
-                                            chat_id: Cow::Owned(chat_id.clone()),
-                                            message_id: msg_id,
-                                        };
-                                        match bot.request(delete_message).await {
-                                            Ok(_) => break,
-                                            Err(e) => {
-                                                warn!("bot request fail: {}", e);
-                                            }
-                                        }
-                                    }
-                                    break;
-                                }
+                            info!("deleting telegram msg id={}", &msg_id);
+                            match bot.delete_message(chat_id, msg_id).await {
+                                Ok(_) => break,
                                 Err(e) => {
-                                    warn!("update to github got error: {}", e);
+                                    warn!("bot request fail: {}", e);
                                 }
                             }
                         }
+                        break;
+                    }
+                    Err(e) => {
+                        warn!("update to github got error: {}", e);
                     }
                 }
-                _ => {}
             }
         }
-    });
-    info!("bot is listening");
-    bot.polling().await;
+        Ok(())
+    }).await;
+
     info!("bot is stopping");
 }
